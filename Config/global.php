d79f0c6ac9a6d08267b569eead796efd2ba41f15
talexfo <titofarinas@gmail.com>
2017-10-20 13:27:02 -0600

    Mantenimiento de Tabla Categoria

--- Config/global.php
+++ Config/global.php
@@ -1,6 +1,6 @@
 <?php 
-//IP de la pc servidor de base de datos
-define("DB_HOST", "localhost");
+//Ip de la pc servidor de base de datos
+define("DB_HOST","localhost");
 
 //Nombre de la base de datos
 define("DB_NAME", "dbsistema");
@@ -8,13 +8,12 @@ define("DB_NAME", "dbsistema");
 //Usuario de la base de datos
 define("DB_USERNAME", "root");
 
-//Contraseña de la base de datos
+//Contraseña del usuario de la base de datos
 define("DB_PASSWORD", "");
 
-//definimos la codificacion de los caracteres
-define("DB_ENCODE", "utf8");
+//definimos la codificación de los caracteres
+define("DB_ENCODE","utf8");
 
 //Definimos una constante como nombre del proyecto
-define("PRO_NOMBRE", "ITVENTAS");
-
+define("PRO_NOMBRE","ITVentas");
 ?>
